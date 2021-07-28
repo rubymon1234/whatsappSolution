ee27ead5668fa581dc0ca8332e8e2d0ce1719e7e
root <root@localhost.localdomain>
2021-07-28 20:38:32 +0530

    spelling corrections

--- resources/views/admin/user/rechargeRequest.blade.php
+++ resources/views/admin/user/rechargeRequest.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container-fluid mt-xl-50 mt-sm-30 mt-15">
    <!-- Row -->
-   <h6 class="hk-sec-title">@yield('title') :: Create Request</h6>
+   <h6 class="hk-sec-title">@yield('title') :: Recharge Request</h6>
    <p class="mb-20"></p>
     <div class="row">
         <div class="col-xl-12">
@@ -15,7 +15,7 @@
                                 <form role="form" action="" method="post" class="form-horizontal">
                                     {{ csrf_field() }}
                                     <div class="form-group row">
-                                        <label for="inputEmail3" class="col-sm-2 col-form-label">User name</label>
+                                        <label for="inputEmail3" class="col-sm-2 col-form-label">Username</label>
                                         <div class="col-sm-10">
                                             <input type="text" name="name" class="form-control" placeholder="Enter User name." value="{{ $user->name }}" readonly="readonly" disabled="disabled">
                                         </div>
@@ -46,7 +46,7 @@
                                                 <br>
                                             @endif
 
-                                            <button type="submit" class="btn btn-info " style="margin-right: 10px;" name="Update" value="Save">Create</button>&nbsp;
+                                            <button type="submit" class="btn btn-info " style="margin-right: 10px;" name="Update" value="Save">Request</button>&nbsp;
                                             <button type="submit" class="btn btn-danger " name="Cancel" value="cancel">Cancel</button>
                                         </div>
                                     </div>
