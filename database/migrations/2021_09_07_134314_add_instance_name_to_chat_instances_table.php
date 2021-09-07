673d5b8fad8691816880ce5decac546499e01490
rubin <rubinmon92@gmail.com>
2021-09-07 14:38:13 +0530

    migration name change

--- database/migrations/2021_09_07_134314_add_instance_name_to_chat_instances_table.php
+++ database/migrations/2021_09_07_134314_add_instance_name_to_chat_instances_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddPhoneNumberToChatInstancesTable extends Migration
+class AddInstanceNameToChatInstancesTable extends Migration
 {
     /**
      * Run the migrations.
