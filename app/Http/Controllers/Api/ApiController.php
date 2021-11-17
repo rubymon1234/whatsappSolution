5e6ee4f6626f4dad80bcb1aef9222b1bad4e00f7
root <root@localhost.localdomain>
2021-11-17 19:56:14 +0530

    basic api added

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,320 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Crypt;
+use App\Models\Role;
+use App\Models\Permission;
+use App\Models\PermissionRole;
+use App\Models\Api;
+use App\Models\Campaign;
+use App\Models\Accounts;
+use App\Models\Instance;
+use App\Helpers\Helper;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Validator;
+
+class ApiController extends Controller
+{
+  public function send(Request $request){
+
+    $api_key 		= $request->key; // campaign name
+    $apiKey 	= Api::where('is_status',1)->where('api_key',$api_key)->first();
+    if ($apiKey){
+      $campaign 		= $request->campaign; // campaign name
+      $instance_token 	=  $apiKey->instance_token;// instance id from database
+      $user_id 	=  $apiKey->user_id;// user id from database
+      $reseller_id 	=  $apiKey->reseller_id;// reseller id from database
+      $message_type 	= $request->type; // message type
+      $mobile 		= $request->mobile; // mobile
+      $message 		= rawurldecode($request->message); // message
+      $uploadfilename = NULL;
+
+      $validResponse = $this->messageTypeValidation($request->type,$request);
+      $extensionValidation = $this->typeValidation($request->type,$request);
+
+      if ($validResponse['status'] ==true && $extensionValidation['status'] ==true){
+        if($message_type !='text'){
+          $uploadfilename = $this->uploadFile($request,'file');
+        }
+        $account 	= Accounts::where('user_id',$user_id)->first();
+        $credit = $account->credits;
+        $csvDetail = $this->createCsv($mobile);
+
+        if ($this->checkCredit($csvDetail['num_count'], $credit, $account)){
+          $num_count = $csvDetail['num_count'];
+          $csv_name = $csvDetail['csv_name'];
+
+          $campaign_start_date = date('Y-m-d');
+          $campaign_start_time = date('H:i:s');
+          if($request->scheduled==1){
+                if(isset($request->date) && isset($request->time)){
+                  $campaign_start_date = $request->date;
+                  $campaign_start_time = $request->time;
+                }else{
+                  //Invalid scheduled date and time
+                }
+          }
+          if(strlen($request->message) >1000){
+              return response()->json([
+                      'success' => false,
+                      'message' =>'success',
+                      'validator' => false,
+                      'response' => 'Message count is more than 1000.',
+                  ]);
+          }
+
+          $getInstance = Instance::where('token',$instance_token)->first();
+          if($getInstance){
+            //Campaign Creation
+            $campaignInsert = new Campaign();
+            $campaignInsert->campaign_name = $campaign;
+            $campaignInsert->user_id = $user_id;
+            $campaignInsert->reseller_id = $reseller_id;
+            $campaignInsert->current_plan_id = NULL;
+            $campaignInsert->leads_file = $csv_name;//csv
+            $campaignInsert->instance_token = $instance_token;
+            $campaignInsert->instance_name = $getInstance->instance_name;
+            $campaignInsert->type = $validResponse['slug'];
+            $campaignInsert->message = rawurlencode($message);
+            $campaignInsert->media_file_name = $uploadfilename;
+            $campaignInsert->count = $num_count;
+
+            if($request->optout =='1'){
+              $campaignInsert->opt_out = 1;
+            }else{
+              $campaignInsert->opt_out = 0;
+            }
+            $campaignInsert->start_at = $campaign_start_date.' '.$campaign_start_time;
+            if($request->scheduled==1){
+              $campaignInsert->is_status = 0; // scheduled
+            }else{
+              $campaignInsert->is_status = 2;
+            }
+            $campaignInsert->save();
+            $last_inserted_id = $campaignInsert->id;
+            //shell_exec('/usr/bin/php /var/www/html/whatsappSolution/cronjob/cronJobNumberPriority.php '.$last_inserted_id.' 2> /dev/null > /dev/null  &');
+
+            if($campaignInsert){
+
+              return response()->json([
+                        'success' => true,
+                        'message' =>'success',
+                        'validator' => false,
+                        'response' => 'Campaign created successfully',
+                    ]);
+            }
+          }else{
+            //Invalid instance token
+            return response()->json([
+                    'success' => true,
+                    'message' =>'success',
+                    'response' => 'Invalid token'
+                ]);
+          }
+
+        }else{
+          // No credits
+        }
+      }else{
+        //Message validation failed
+        return response()->json([
+                'success' => true,
+                'message' =>'success',
+                'response' => 'Invalid extension or message type'
+            ]);
+      }
+
+
+
+    }else{
+      //Invalid API Key
+    }
+
+  }
+
+  public function checkCredit($count, $credit, $account){
+    if ($credit >= $count){
+      //update credit
+      $updateAccount = Accounts::find($account->id);
+      $updateAccount->credits = $credit-$count;
+      $updateAccount->save();
+
+      return true;
+    }
+    return false;
+  }
+
+  public function createCsv($contacts){
+    $csvDetail = array();
+    $token = Helper::generateUniqueId();
+    $leadPath = public_path('/uploads/csv/');
+    $csvName = str_replace(" ","",$token).'.csv';
+    $file = $leadPath.$csvName;
+
+  if ($contacts) {
+    $contacts = explode(",",trim($contacts));
+    $csvDetail['num_count'] = count($contacts);
+    $fp = fopen("$file", 'w');
+    foreach($contacts as $line){
+      $val = explode(",",$line);
+      fputcsv($fp, $val);
+    }
+    fclose($fp);
+    $csvDetail['csv_name'] = $csvName;
+    return $csvDetail;
+  }
+  return false;
+  }
+  public function uploadFile($request,$type){
+      $ext = strtolower($request->file($type)->getClientOriginalExtension());
+      $token = Helper::generateUniqueId();
+      $microtime = str_replace(" ","",$token);
+      $filename = '_Media_'. $type . $microtime . '.' . $ext;
+  $request->file($type)->move(public_path('uploads/media/'), $filename);
+  return $filename;
+  }
+public function messageTypeValidation($message_type,$request){
+
+  $errorMessage = '';
+  $arrayMessage['status'] = '';
+  $arrayMessage['message'] = '';
+
+  if($message_type =='text'){ //only text
+    if(isset($request->message)){
+      $arrayMessage['slug'] = 'text';
+      $arrayMessage['status'] = true;
+    }else{
+      $arrayMessage['status'] = false;
+      $arrayMessage['message'] = 'Message field is mandatary';
+    }
+  }elseif ($message_type =='image') { //text + photo
+    if(isset($request->photo) && isset($request->message)){
+      $arrayMessage['slug'] = 'media-text';
+      $arrayMessage['status'] = true;
+    }else{
+      $arrayMessage['status'] = false;
+      $arrayMessage['message'] = 'Image and message fields are mandatary';
+    }
+  }elseif($message_type =='video'){ //text + video_file
+
+    if(isset($request->video_file) && isset($request->message)){
+      $arrayMessage['slug'] = 'media-text';
+      $arrayMessage['status'] = true;
+    }else{
+      $arrayMessage['status'] = false;
+      $arrayMessage['message'] = 'Video and message fields are mandatary';
+    }
+  }elseif($message_type =='audio'){ //audio_file only
+    if(isset($request->audio_file)){
+      $arrayMessage['slug'] = 'media';
+      $arrayMessage['status'] = true;
+    }else{
+      $arrayMessage['status'] = false;
+      $arrayMessage['message'] = 'Audio field is mandatary';
+    }
+  }elseif ($message_type =='document') { // doc_file only
+    if(isset($request->doc_file)){
+      $arrayMessage['slug'] = 'media';
+      $arrayMessage['status'] = true;
+    }else{
+      $arrayMessage['status'] = false;
+      $arrayMessage['message'] = 'Document field is mandatary';
+    }
+  }else{
+    $arrayMessage['status'] = false;
+    $arrayMessage['message'] = 'Invalid message type';
+  }
+  return $arrayMessage;
+}
+public function typeValidation($message_type,$request){
+  //$arrayMessage = array();
+  $arrayMessage['status'] = '';
+  $arrayMessage['message'] = '';
+  if($message_type =='text'){ //only text
+    if(isset($request->message)){
+      $arrayMessage['status'] = true;
+    }else{
+      $arrayMessage['status'] = false;
+      $arrayMessage['message'] = 'Message field is mandatary';
+    }
+  }elseif ($message_type =='image') { //text +photo
+    if(isset($request->photo)){
+      $extension = ['jpeg','png','jpg'];
+      $ext = strtolower($request->file('file')->getClientOriginalExtension());
+      $fileSize = $request->file('file')->getSize();
+      if($fileSize <=4000000){
+        if(in_array($ext, $extension)){
+          $arrayMessage['status'] = true;
+        }else{
+          $arrayMessage['status'] = false;
+          $arrayMessage['message'] = 'Image accepted format are jpg, png, jpeg';
+        }
+      }else{
+        $arrayMessage['status'] = false;
+        $arrayMessage['message'] = 'Image maximum file size is 4MB';
+      }
+    }
+  }elseif($message_type =='video') {
+    if(isset($request->video_file)){
+      $extension = ['mp4','3gp'];
+      $ext = strtolower($request->file('file')->getClientOriginalExtension());
+      $fileSize = $request->file('file')->getSize();
+      if($fileSize <=4000000){
+
+        if(in_array($ext, $extension)){
+          $arrayMessage['status'] = true;
+        }else{
+          $arrayMessage['status'] = false;
+          $arrayMessage['message'] = 'Video accepted format are mp4, 3gpp';
+        }
+      }else{
+        $arrayMessage['status'] = false;
+        $arrayMessage['message'] = 'Video maximum file size is 4MB';
+      }
+    }
+  }elseif($message_type =='audio') {
+    if(isset($request->audio_file)) {
+      $extension = ['aac','mp3','amr'];
+      $ext = strtolower($request->file('file')->getClientOriginalExtension());
+      $fileSize = $request->file('file')->getSize();
+      if($fileSize <=4000000){
+
+        if(in_array($ext, $extension)){
+          $arrayMessage['status'] = true;
+        }else{
+          $arrayMessage['status'] = false;
+          $arrayMessage['message'] = 'Accepted audio format is aac, mp3, amr';
+        }
+      }else{
+        $arrayMessage['status'] = false;
+        $arrayMessage['message'] = 'Audio maximum file size is 4MB';
+      }
+    }
+  }elseif ($message_type =='document') {
+    $extension = ['doc', 'xls' , 'ppt', 'docx', 'xlsx', 'pptx', 'pdf', 'txt'];
+    $ext = strtolower($request->file('file')->getClientOriginalExtension());
+    $fileSize = $request->file('file')->getSize();
+    if($fileSize <=4000000){
+
+      if(in_array($ext, $extension)){ //&& $fileSize <=4000000
+        $arrayMessage['status'] = true;
+      }else{
+        $arrayMessage['status'] = false;
+        $arrayMessage['message'] = 'Accepted document format are doc, xls, ppt, docx, xlsx, pptx, pdf, txt';
+      }
+    }else{
+      $arrayMessage['status'] = false;
+      $arrayMessage['message'] = 'Document maximum file size is 4MB';
+    }
+  }else{
+    $arrayMessage['status'] = false;
+    $arrayMessage['message'] = 'Invalid message type';
+  }
+  return $arrayMessage;
+}
+}
---
+++ database/migrations/2021_11_16_190721_create_apis_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateApisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apis', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('reseller_id')->unsigned();
+            $table->string('api_key');
+            $table->string('instance_token');
+            $table->smallInteger('is_status')->nullable()->comment('0-Not Active,1-Active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apis');
+    }
+}
