3fcd097d2d0113ef7ad91d8d45c1178db2d0c8c6
rubin <rubinmon92@gmail.com>
2022-08-14 21:47:27 +0530

    api

--- app/Http/Controllers/Api/Common/IncomingMessageCaptureController.php
+++ app/Http/Controllers/Api/Common/IncomingMessageCaptureController.php
@@ -34,8 +34,10 @@ class IncomingMessageCaptureController extends Controller
         $InsertInboundMessage->instance_token = $request->token;
         //get token 
         $instanceToken = Instance::where('token', $request->token)->first();
+
         $InsertInboundMessage->user_id = $instanceToken->user_id;
         $InsertInboundMessage->reseller_id = $instanceToken->reseller_id;
+        $InsertInboundMessage->number = explode("@",$request->from)[0];
         //sent with web_hook url
         if($instanceToken->web_hook_url){
             $sentWebHookURLResponse = $this->sentWebHookURL($request,$instanceToken->web_hook_url,'POST');
@@ -44,10 +46,12 @@ class IncomingMessageCaptureController extends Controller
                 $InsertInboundMessage->web_hook_url_response = $response;
             }
         }
+
         $InsertInboundMessage->message = $request->text->body; //message json encode value
         $InsertInboundMessage->messaging_product = $request->messaging_product;
-        $InsertInboundMessage->json_data = $request;
+        $InsertInboundMessage->json_data = json_encode($request);
         $InsertInboundMessage->save();
+        return true;
     }
     public function sentWebHookURL($request,$endpoint,$method){
 
